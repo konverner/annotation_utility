3d4dca79c52c871003107fcf02bb6d457cd82fb0
Constantin Werner <52051213+conwerner@users.noreply.github.com>
2022-01-05 23:08:36 +0700

    updated to 0.3

--- src/main.py
+++ src/main.py
@@ -5,25 +5,18 @@ from tkinter import messagebox
 from PIL import Image, ImageTk
 from worker import *
 from make_match import *
-from yaspeller import check
 
-worker1 = Worker()
+
 global IMAGE_PATH
-global LANG
 
+LANG = 'ru'
+SHOW_ERRORS_ONLY = False
 RED_COLOR = Image.new('RGB', (110, 5), (200, 0, 0))
 WHITE_COLOR = Image.new('RGB', (110, 5), (250, 250, 250))
 
 
 def callback():
     webbrowser.open_new("https://github.com/conwerner/annotation_utility")
-
-# check spelling of phrase
-def spelling_correct(phrase, lang):
-    if '-' not in phrase:
-        res = check(phrase, lang=lang)
-        return res.is_ok
-    return True
 
 # upload images (PATH DIR) and its annotation document (PATH TXT)
 def upload():
@@ -38,22 +31,33 @@ def upload():
     e3.config(state='disabled')
 
 # next entity
-def next(event=None,update=True):
+def next(update=True):
+    global SHOW_ERRORS_ONLY
     new_label = e1.get()
     IMAGE_PATH = worker1.get_current()[0]
-    k = worker1.get_curr_idx()
     if update:
-        worker1.update(IMAGE_PATH,new_label)
-    IMAGE_PATH, label = worker1.next()
+        worker1.update(IMAGE_PATH, new_label)
 
-    if not spelling_correct(label, LANG):
+    IMAGE_PATH, entity = worker1.next()
+    if SHOW_ERRORS_ONLY:
+        i = 0
+        while entity['spelling_ok']:
+            IMAGE_PATH, entity = worker1.next()
+            i += 1
+            if i > worker1.N:
+                messagebox.showinfo('INFO', 'There is no label with spelling errors')
+                SHOW_ERRORS_ONLY = False
+                break
+    if not entity['spelling_ok']:
         render = ImageTk.PhotoImage(RED_COLOR)
     else:
         render = ImageTk.PhotoImage(WHITE_COLOR)
+
     img1 = tk.Label(master, image=render)
     img1.image = render
     img1.place(x=230, y=200)
 
+    k = worker1.get_curr_idx()
     N = worker1.N
     l3.configure(text=f'{k}/{N}')
     load = Image.open(IMAGE_PATH)
@@ -62,17 +66,27 @@ def next(event=None,update=True):
     img.image = render
     img.place(x=120, y=10)
     e1.delete(0, 'end')
-    e1.insert('end',label)
+    e1.insert('end',entity['label'])
 
 # previous entity
 def prev(event=None):
+    global SHOW_ERRORS_ONLY
     new_label = e1.get()
     IMAGE_PATH = worker1.get_current()[0]
-    k = worker1.get_curr_idx()
     worker1.update(IMAGE_PATH, new_label)
-    IMAGE_PATH, label = worker1.previous()
 
-    if not spelling_correct(label, LANG):
+    IMAGE_PATH, entity = worker1.previous()
+    if SHOW_ERRORS_ONLY:
+        i = 0
+        while entity['spelling_ok']:
+            IMAGE_PATH, entity = worker1.previous()
+            i += 1
+            if i > worker1.N:
+                messagebox.showinfo('INFO', 'There is no label with spelling errors')
+                SHOW_ERRORS_ONLY = False
+                break
+
+    if not entity['spelling_ok']:
         render = ImageTk.PhotoImage(RED_COLOR)
     else:
         render = ImageTk.PhotoImage(WHITE_COLOR)
@@ -80,6 +94,7 @@ def prev(event=None):
     img1.image = render
     img1.place(x=230, y=200)
 
+    k = worker1.get_curr_idx()
     N = worker1.N
     l3.configure(text=f'{k}/{N}')
     load = Image.open(r'{}'.format(IMAGE_PATH))
@@ -88,7 +103,7 @@ def prev(event=None):
     img.image = render
     img.place(x=120, y=10)
     e1.delete(0, 'end')
-    e1.insert('end', label)
+    e1.insert('end', entity['label'])
 
 # save changes on images and its annotation
 def save():
@@ -101,27 +116,34 @@ def save():
     messagebox.showinfo('INFO', f'{n} images have been saved')
 
 # remove pair (image, label)
-def remove(event=None):
+def remove():
     worker1.remove_current()
     next(update=False)
+
+
+def switch_show_mode():
+    global SHOW_ERRORS_ONLY
+    SHOW_ERRORS_ONLY = not SHOW_ERRORS_ONLY
+
+worker1 = Worker()
 
 master = tk.Tk()
 master.title('Annotation Utility')
 master.geometry('550x380')
 
-LANG = 'ru'
 e1 = tk.Entry(master)
 e1.place(x=220,y=180)
+
 load = Image.new('RGB', (324, 128), (random.randint(0,255), random.randint(0,255), random.randint(0,255)))
 render = ImageTk.PhotoImage(load)
 img = tk.Label(master,image=render)
 img.image = render
 img.place(x=110, y=10)
-b1=tk.Button(master,
-          text='previous',
-          command=prev).place(x=220,y=210)
-b2=tk.Button(master,
-          text='next',command=next).place(x=300,y=210)
+
+b1 = tk.Button(master, text='previous', command=prev).place(x=220, y=210)
+b2 = tk.Button(master, text='next', command=next).place(y=210, x=300)
+
+c1 = tk.Checkbutton(master, text = "show entities with spelling errors only", command=switch_show_mode).place(x=190, y=155)
 
 l1 = tk.Label(master,text='img dir')
 l1.place(x=160,y=260)
--- src/worker.py
+++ src/worker.py
@@ -2,6 +2,7 @@
 Class for handling data: switch, remove, change, etc.
 """
 import collections
+from yaspeller import check
 
 class Worker:
     def __init__(self):
@@ -20,41 +21,48 @@ class Worker:
         self.name2label[self.current] = None
         self.current += 1
 
-    def upload(self,PATH_TXT,PATH_DIR):
+    def upload(self, PATH_TXT, PATH_DIR):
         name2label = dict()
         f = open(PATH_TXT, 'r', encoding='utf-8')
         txt = f.read()
         lines = txt.split('\n')
-        for line in lines:
+        N = len(lines)
+        print("THIRD STEP: checking spelling")
+        for i,line in enumerate(lines):
+            if (i%1000 == 0):
+                print(f"[{i}\{N}] labels has been processed")
             try:
-                name2label[PATH_DIR + '\\' + line.split('\t')[0]] = line.split('\t')[1]
+                label = line.split('\t')[1]
+                PATH = PATH_DIR + '\\' + line.split('\t')[0]
+                name2label[PATH] = {'label' : label, 'spelling_ok' : self.spelling_correct(label)}
             except:
-                print("ERROR (bad line):",line)
-
+                print("ERROR (bad line):", line)
         name2label = collections.OrderedDict(sorted(name2label.items()))
 
         self.name2label = list(name2label.items())
         self.first = self.name2label[0]
         self.N = len(self.name2label)
         f.close()
+        print("END")
         return len(name2label)
 
-    def save(self,PATH_TXT,PATH_DIR):
+    def save(self, PATH_TXT, PATH_DIR):
         counter = 0
-        file_txt = open(PATH_TXT,'w')
+        file_txt = open(PATH_TXT, 'w')
         the_last = self.name2label[-1]
         for item in self.name2label:
             if item != None:
                 counter += 1
                 if item != the_last:
                     path = item[0]
-                    path = path.replace(PATH_DIR,'')
+                    path = path.replace(PATH_DIR, '')
+                    label = item[1]['label']
                     try:
-                        file_txt.write((path[1:]+'\t'+item[1]+'\n').encode('utf-8',"ignore").decode())
+                        file_txt.write((path[1:]+'\t'+label+'\n').encode('utf-8', "ignore").decode())
                     except:
-                        print("ERROR:",item[1])
-        path = the_last[0].replace(PATH_DIR,'')
-        file_txt.write((path[1:]+'\t'+the_last[1]).encode('utf-8',"ignore").decode())
+                        print("ERROR:", label)
+        path = the_last[0].replace(PATH_DIR, '')
+        file_txt.write((path[1:]+'\t'+the_last[1]['label']).encode('utf-8', "ignore").decode())
         file_txt.close()
         counter += 1
         return counter
@@ -64,9 +72,9 @@ class Worker:
         if self.current >= len(self.name2label):
             self.current = 0
         while self.name2label[self.current] == None:
-            print('skip',self.current)
+            print('skip', self.current)
             self.current += 1
-        print('next:',self.name2label[self.current])
+        print('next:', self.name2label[self.current][0], self.name2label[self.current][1]['label'])
         return self.name2label[self.current]
 
     def get_current(self):
@@ -83,11 +91,20 @@ class Worker:
             self.current -= 1
         return self.name2label[self.current]
 
-    def update(self,name,new_label):
+    def update(self, name, new_label):
         print('udpate:', name, new_label)
         for i in range(len(self.name2label)):
             if self.name2label[i] != None:
                 if self.name2label[i][0] == name:
                     self.name2label[i] = list(self.name2label[i])
-                    self.name2label[i][1] = new_label
+                    self.name2label[i][1]['label'] = new_label
+                    self.name2label[i][1]['spelling_ok'] = self.spelling_correct(new_label)
                     self.name2label[i] = tuple(self.name2label[i])
+
+    # check spelling of phrase
+    def spelling_correct(self, phrase, lang='ru'):
+        phrase = phrase.strip()
+        if '-' not in phrase and phrase != '':
+            res = check(phrase, lang=lang)
+            return res.is_ok
+        return True
